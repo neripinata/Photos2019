463f65d64be26a2b5914cbeda4ad8a08267e2522
Neri <45918587+neripinata@users.noreply.github.com>
2020-01-20 10:30:37 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import re
 import random
 
 dataset = "qualification_round_2019.in/a_example.txt"
-
+retry = True
 
 f = open(dataset, "r")
 #print(f.read())
 
 text = f.read()
 
+output = []
 
 rx = re.compile(r'\s{2,}|\n+')
 simple_space = re.compile(r'\s{1,}|\n+')
@@ -27,10 +28,40 @@ print(categories)
 
 print(len(categories))
 
+def replace_value_with_definition(key_to_find, definition):
+    for key in categories.keys():
+        if key == key_to_find:
+            categories[key] = definition
+
+
+
 for i in range(len(categories)):
-    number = random.randrange(len(categories))
-    types = (rx.split(text))
-    etiquetas = (simple_space.split(categories[number]))
-    print(etiquetas)
+    for i in range(len(categories) * 10):
+        number = random.randrange(len(categories))
+        types = (rx.split(text))
+        photo_one = (simple_space.split(categories[number]))
+        for i in range(len(categories)):
+            number_dos = random.randrange(len(categories))
+            while number_dos == number:
+                number_dos = random.randrange(len(categories))
+            types = (rx.split(text))
+            photo_two = (simple_space.split(categories[number_dos]))
+        
+    #print(photo_one)
+    #print(photo_two)
+
+    similarities =  set(photo_one[2:]) & set(photo_two[2:])
+
+    if not similarities:
+        pass
+    else:
+        output.append(number)
+        output.append(number_dos)
+        replace_value_with_definition(number, 'Used')
+        replace_value_with_definition(number_dos, 'Used')
+        retry = False
+
+
+print(output)
 
 #print(types)
